bba6c81b7a3b6257756a8a65863a05738ab31252
dev-from-cbd <veryfatwombat@gmail.com>
2023-12-13 06:03:20 +1030

    update

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -5,3 +5,22 @@ interface DOMList {
     clear(): void,
     render(FullList: FullList): void,
 }
+
+export default class ListTemplate implements DOMList {
+    ul: HTMLUListElement;
+    private constructor() {
+        this.ul = document.getElementById('listItems') as HTMLUListElement;
+    }
+
+    clear(): void {
+        this.ul.innerHTML = '';
+    }
+
+    render(fullList: Full): void {
+        this.clear()
+
+        fullList.list.forEach(item => {
+        
+        })
+    }
+}
